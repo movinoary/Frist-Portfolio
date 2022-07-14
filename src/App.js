339e36aa64103eef1d214db2e02451ad5f1ad93d
vinoarystio <rocevo.id@gmail.com>
2022-07-14 18:04:12 +0700

    init

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
 
   return (
     <>
-      <Components.Loading />
-      {/* {isLoadng ? (
+      {isLoadng ? (
+        <Components.Loading />
       ) : (
         <>
           <Components.Nav />
@@ -53,7 +53,7 @@ function App() {
             </a>
           </div>
         </>
-      )} */}
+      )}
     </>
   );
 }
